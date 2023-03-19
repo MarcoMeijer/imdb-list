4f04be7e7102e34d744a22aa3aa4fc0d76f47fef
Marco Meijer <marcomeijerm@gmail.com>
2023-03-19 20:05:47 +0100

    style: movie page style

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,6 +1,7 @@
-import RouteButton from "@/components/button/RouteButton";
 import { Movie } from "@/types/Movie";
 import Image from "next/image";
+import styles from "./page.module.css";
+import StarIcon from "@/components/icons/StarIcon";
 
 export interface MoviePageProps {
   params: { id: string };
@@ -23,21 +24,44 @@ export default async function MoviePage({ params }: MoviePageProps) {
   }
 
   return (
-    <div>
-      <Image src={movie.image_url} width={260} height={386} alt={""} />
-      <h1>
-        {movie.year} - {movie.name}
-      </h1>
-      <h1>IMDB {movie.rating}/10</h1>
-      <p>{movie.desc}</p>
-      <h1>Directors</h1>
-      {movie.directors.map((actor, i) => (
-        <p key={i}>{actor}</p>
-      ))}
-      <h1>Actors</h1>
-      {movie.actors.map((actor, i) => (
-        <p key={i}>{actor}</p>
-      ))}
-    </div>
+    <main className={styles.movieInfo}>
+      <h1 className={styles.title}>{movie.name}</h1>
+      <p className={styles.underTitle}>
+        {movie.year}
+        <div className={styles.rating}>
+          <StarIcon
+            fill="#fbd341"
+            width={26}
+            height={26}
+            style={{ marginBottom: 4, marginRight: 4 }}
+          />
+          {movie.rating.toFixed(1)}/10
+        </div>
+      </p>
+      <div className={styles.content}>
+        <Image src={movie.image_url} width={260} height={386} alt={""} />
+        <div className={styles.description}>
+          <p>{movie.desc}</p>
+          <div className={styles.section}>
+            <h1>Directors</h1>
+            {movie.directors.map((director, i) => (
+              <p key={i}>{director}</p>
+            ))}
+          </div>
+          <div className={styles.section}>
+            <h1>Actors</h1>
+            {movie.actors.map((actor, i) => (
+              <p key={i}>{actor}</p>
+            ))}
+          </div>
+          <div className={styles.section}>
+            <h1>Genre</h1>
+            {movie.genre.map((genre, i) => (
+              <p key={i}>{genre}</p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
